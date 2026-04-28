253a7546b2fc720513904204ecdd098e2c88b0c5
Shu Zhu <sz9922@psu.edu>
2026-04-28 19:14:01 -0400

    Update final_project.py
    Completing the functions and start to check

--- final_project.py
+++ final_project.py
@@ -14,30 +14,30 @@ This is a simple but effective project
 to practice logic building, loops, and input handling.
 
 Requirements
-. Random Number Generation:
+. Random Number Generation:v
     Game starts by a random number generation by the
     program (e.g., between 1 and 100)
-. User Input:
+. User Input:v
     Player guesses the number
     (using the input() function
     and convert to int())
     User is allowed multiple attempts to guess the number
-· Provide feedback:
+· Provide feedback:v
     o "Too high": If the guess is greater than the number generated, it prompts the
     user to guess a lower number.
     o "Too low": If the guess is lower than the number generated, it prompts the
     user to guess a higher number.
     o "Correct!": If the guess matches, game displays "Congratulations!" and ends
     the game.
-. Attempt Tracking:
+. Attempt Tracking:v
     Program should keep track of the number of attempts
 . End the game when the correct number is guessed
 
 Implementation Details
-. Use: random.randint(1, 100)
-. Use a loop to continue until the correct guess
-· Validate user input (ensure it is a number)
-. Store and display the number of attempts
+. Use: random.randint(1, 100)v
+. Use a loop to continue until the correct guess v
+· Validate user input (ensure it is a number) v
+. Store and display the number of attempts v
 
 Suggested Enhancements (Optional)
 . Difficulty levels (easy, medium, hard)
@@ -77,13 +77,34 @@ def userInput():
 ##    user to guess a higher number.
 ##    o "Correct!": If the guess matches, game displays "Congratulations!" and ends
 ##    the game.
-def checkGuess(a, guess):
-    if guess > a:
-        return("Too high. Try to guess a lower number. ↘↘↘")
-    elif guess < a:
-        return("Too low. Try to guess a higher number. ↗↗↗")
+def feedback(iniNum, guess):
+    if guess > iniNum:
+        print("Too high. Try to guess a lower number. ↘↘↘")
+        return False
+    elif guess < iniNum:
+        print("Too low. Try to guess a higher number. ↗↗↗")
+        return False
     else:
-        return("Correct!")
+        print("Correct!")
+        return True
 
 
-main
+def main():
+    iniNum = randomNum()
+    #for check purpose
+    print(iniNum)
+    
+    guessCount = 0 #Attempt Tracking
+    status = False
+    
+    while status != True:
+        guess = userInput()
+        
+        if feedback(iniNum, guess) == False:
+            guessCount += 1
+        else:
+            print(f"Your total number of guess is: {guessCount}")
+    
+
+if __name__ == "__main__":
+    main()
